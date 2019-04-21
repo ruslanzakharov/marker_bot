76413f1a5bb3555357dc2165b573339d021c69dc
kinokam <45312192+kinokam@users.noreply.github.com>
2019-04-21 21:29:08 +0300

    Update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, request
 import logging
 import json
+import requests
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO, filename='app.log',
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+session_storage = {}
+
+SKILL_ID = 'a38dddc2-b89a-4a22-8b93-f70ff53d31ed'
 
 
 @app.route('/post', methods=['POST'])
@@ -29,7 +34,52 @@ def main():
 
 def handle_dialog(res, req):
     """Обрабатывает запрос"""
-    pass
+
+    if req['session']['new']:
+        res['response']['text'] = 'Введите координаты'
+
+    req_text = req['request']['original_utterance']
+    if not session_storage:
+        session_storage['закладка1'] = req_text
+
+        return
+
+    if session_storage:
+        map = set_marker(session_storage['закладка1'])
+
+        res['card'] = {
+            'type': 'BigImage',
+            'image_id': map,
+        }
+
+        return
+
+
+def set_marker(coord):
+    """Создает карту с меткой"""
+
+    # Получаем изображение карты с меткой
+    url_static_api = 'https://static-maps.yandex.ru/1.x/'
+
+    params = {
+        'l': 'sat,sk1',
+        'll': 'coord',
+    }
+
+    map = requests.get(url_static_api, params)
+    print(map)
+
+    # Размещаем изображение в Яндекс.Диалоги и получаем id
+    url_ya_dialogs = f'https://dialogs.yandex.net/api/v1/skills/{SKILL_ID}/images'
+
+    files = {'file': map}
+    headers = {
+        'Authorization': 'QAuth AQAAAAATNA8lAAT7o-7tTToNjUOljgg7VeV1pdY'
+    }
+
+    id = requests.post(url_ya_dialogs, files=files, headers=headers).json()
+
+    return id['image']['id']
 
 
 if __name__ == '__main__':
