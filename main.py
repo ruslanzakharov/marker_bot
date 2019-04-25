ecd0dca10d68d2d0041e6dc2e0df46de1c6280ee
ruslanzakharov <ruslanzakharov3008@gmail.com>
2019-04-25 11:23:26 +0300

    Реализована регистрация и вход пользователя

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import logging
 import json
 import requests
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite: ///main.db'
 db = SQLAlchemy(app)
 
 logging.basicConfig(level=logging.INFO, filename='app.log',
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
-session_storage = {}
+session_storage = {
+    'Регистрация': False,
+    'Вход': False
+}
 
 SKILL_ID = 'a38dddc2-b89a-4a22-8b93-f70ff53d31ed'
 
@@ -63,10 +67,78 @@ def handle_dialog(res, req):
     """Обрабатывает запрос"""
 
     if req['session']['new']:
-        res['response']['text'] = 'Введите координаты'
+        res['response']['text'] = 'Зарегистрируйтесь или войдите'
+        res['response']['buttons'] = [
+            {
+                'title': 'Регистрация',
+                'payload': {'Регистрация': True},
+                'hide': True
+            },
+            {
+                'title': 'Вход',
+                'payload': {'Вход': True},
+                'hide': True
+            }
+        ]
+
         return
 
     req_text = req['request']['original_utterance']
+
+    # Регистрация
+    if req_text == 'Регистрация' and req['request']['payload']:
+        res['response']['text'] = 'Введите имя и пароль через пробел'
+        session_storage['Регистрация'] = True
+
+        return
+
+    if session_storage['Регистрация']:
+        username, password = req_text.split()
+
+        password_hash = generate_password_hash(password)
+
+        user = User(
+            username=username,
+            password_hash=password_hash
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        session_storage['Регистрация'] = False
+
+        res['response']['text'] = 'Вы зарегистрировались, можете войти'
+        res['response']['buttons'] = [
+            {
+                'title': 'Вход',
+                'payload': {'Вход': True},
+                'hide': True
+            }
+        ]
+
+    # Вход
+    if req_text == 'Вход' and req['request']['payload']:
+        res['response']['text'] = 'Введите имя и пароль через пробел'
+        session_storage['Войти'] = True
+
+        return
+
+    if session_storage['Войти']:
+        username, password = req_text.split()
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            session['username'] = user.username
+
+        res['response']['text'] = 'Вы вошли'
+        res['response']['buttons'] = [
+            {
+                'title': 'Вход',
+                'payload': {'Вход': True},
+                'hide': True
+            }
+        ]
+
     map = set_marker(req_text)
 
     res['response']['text'] = 'Фото'
